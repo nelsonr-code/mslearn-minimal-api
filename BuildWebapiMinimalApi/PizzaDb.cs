5b7e0c5941af29313c0e32170ea8dd71c77e8e65
Nelson Rivas <nelson.rivas.ucv+code@gmail.com>
2023-07-15 20:30:33 -0400

    Add EF in-memory database

---
+++ BuildWebapiMinimalApi/PizzaDb.cs
@@ -0,0 +1,6 @@
+namespace BuildWebapiMinimalApi;
+
+public class PizzaDb
+{
+    
+}
--- BuildWebapiMinimalApi/Program.cs
+++ BuildWebapiMinimalApi/Program.cs
@@ -1,8 +1,12 @@
-using BuildWebapiMinimalApi.DB;
+using Microsoft.EntityFrameworkCore;
+using BuildWebapiMinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => 
+    options.UseInMemoryDatabase("items")
+    );
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new()
@@ -22,10 +26,33 @@ app.UseSwaggerUI(c =>
 });
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/pizzas/{id:int}", PizzaDb.GetPizza);
-app.MapGet("/pizzas", PizzaDb.GetPizzas);
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDb.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDb.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id:int}", (int id) => PizzaDb.RemovePizza(id));
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizzas", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    
+    return Results.Created($"/pizzas/{pizza.Id}", pizza);
+});
+app.MapGet("/pizza/{id:int}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPut("/pizza/{id:int}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    
+    return Results.Ok(pizza);
+});
+app.MapDelete("/pizzas/{id:int}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    
+    return Results.Ok();
+});
 
 app.Run();
